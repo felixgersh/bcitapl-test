1b18ea30b37dc82a814a4774978cfed30966eb67
Felix Gershunov <felix.gershunov@gmail.com>
2020-02-25 13:21:56 +0000

    first dirty version of CommandLineParser used

--- user_upload.php
+++ user_upload.php
@@ -1,2 +1,15 @@
 <?php
 
+require_once 'CommandLineParser.php';
+
+try {
+    $parser = new CommandLineParser($argc, $argv);
+    $parser->printOptions();
+} catch (Exception $e) {
+    if ($e->getCode() == -1) {
+        CommandLineParser::displayHelp();
+    } else {
+        echo "\nError: ".$e->getMessage()."\n";
+        exit(1);
+    }
+}
