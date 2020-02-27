440191e13177c44b77f42c8f048d93d9df13c758
Felix Gershunov <felix.gershunov@gmail.com>
2020-02-27 11:59:29 +0000

    help message improvements; whitespace correction

--- CommandLineParser.php
+++ CommandLineParser.php
@@ -19,19 +19,19 @@ class CommandLineParser
             //echo $argv[$i]."\n";
             if ($argv[$i] == '--file') {
                 if ($i + 1 == $argc) {
-                    throw new Exception('CSV filename should be specified');                
+                    throw new Exception('CSV filename should be specified');
                 }
                 $filename = $argv[++$i];
                 if ((strlen($filename) < 1) || ($filename[0] == '-')) {
-                    throw new Exception('CSV filename should not be empty and should not start with -');                
+                    throw new Exception('CSV filename should not be empty and should not start with -');
                 }
                 $this->options['--file'] = $filename;
             } elseif ($argv[$i] == '--create_table') {
-                $this->options['--create_table'] = true; 
+                $this->options['--create_table'] = true;
             } elseif ($argv[$i] == '--dry_run') {
-                $this->options['--dry_run'] = true; 
+                $this->options['--dry_run'] = true;
             } elseif ($argv[$i] == '--help') {
-                throw new Exception('', -1);                
+                throw new Exception('', -1);
             } elseif ($this->assignShortOption($i, '-h', 'MySQL host')) {
             } elseif ($this->assignShortOption($i, '-u', 'MySQL username')) {
             } elseif ($this->assignShortOption($i, '-p', 'MySQL password')) {
@@ -56,11 +56,11 @@ class CommandLineParser
     {
         if ($this->argv[$i] == $optionName) {
             if ($i + 1 == $this->argc) {
-                throw new Exception("$optionMeaning should be specified");                
+                throw new Exception("$optionMeaning should be specified");
             }
             $paramValue = $this->argv[++$i];
             if ($paramValue[0] == '-') {
-                throw new Exception("If your $optionMeaning should start with '-', specify it without space after $optionName");                
+                throw new Exception("If your $optionMeaning should start with '-', specify it without space after $optionName");
             }
             $this->options[$optionName] = $paramValue;
             return true;
@@ -77,7 +77,7 @@ class CommandLineParser
     {
         foreach ($this->shortOptions as $option) {
             if (!isset($this->options[$option[0]])) {
-                throw new Exception($option[1].' should be specified');    
+                throw new Exception($option[1].' should be specified');
             }
         }
     }
@@ -85,12 +85,12 @@ class CommandLineParser
     public static function displayHelp()
     {
         $helpMessage = <<<HELP
---file [csv file name] - this is the name of the CSV to be parsed
+--file [csv file name] - this is the name of the CSV file to be parsed
+--dry_run - this can be used with the --file directive in case you want to run
+            the script, but not insert into the DB. All other functions
+            will be executed, but the database will not be altered
 --create_table - this will cause the MySQL users table to be built
                  (and no further action will be taken)
---dry_run - this will be used with the --file directive in case you want
-            to run the script but not insert into the DB. All other functions
-            will be executed, but the database will not be altered
 -u - MySQL username
 -p - MySQL password
 -h - MySQL host
@@ -98,19 +98,7 @@ class CommandLineParser
 HELP;
         echo "\n".$helpMessage."\n";
     }
-    /*
-      The PHP script should include these command line options (directives):
-. --file [csv file name] . this is the name of the CSV to be parsed
-. --create_table . this will cause the MySQL users table to be built (and no further
-. action will be taken)
-. --dry_run . this will be used with the --file directive in case we want to run the
-script but not insert into the DB. All other functions will be executed, but the
-database won't be altered
-. -u . MySQL username
-. -p . MySQL password
-. -h . MySQL host
-. --help . which will output the above list of directives with details.
-    */
+
     public function printOptions()
     {
         print_r($this->options);
