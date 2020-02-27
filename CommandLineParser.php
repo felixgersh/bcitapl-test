fa0870fd7670009f8c471adb78b85babb5842537
Felix Gershunov <felix.gershunov@gmail.com>
2020-02-27 22:55:38 +0000

    constants is used for command line options; various code improvements

--- CommandLineParser.php
+++ CommandLineParser.php
@@ -1,12 +1,17 @@
 <?php
 
+define('CREATE_TABLE_OPTION', '--create_table');
+define('DRY_RUN_OPTION', '--dry_run');
+define('CSV_FILE_OPTION', '--file');
+define('HELP_OPTION', '--help');
+
 class CommandLineParser
 {
     private $options = [];
     private $shortOptions = [
-        ['-h', 'MySQL host'],
-        ['-u', 'MySQL username'],
-        ['-p', 'MySQL password']
+        '-h' => 'MySQL host',
+        '-u' => 'MySQL username',
+        '-p' => 'MySQL password'
     ];
     private $argc, $argv;
 
@@ -14,10 +19,8 @@ class CommandLineParser
     {
         $this->argc = $argc;
         $this->argv = $argv;
-        //print_r([$argc, $argv]);
         for ($i = 1; $i < $argc; $i++) {
-            //echo $argv[$i]."\n";
-            if ($argv[$i] == '--file') {
+            if ($argv[$i] == CSV_FILE_OPTION) {
                 if ($i + 1 == $argc) {
                     throw new Exception('CSV filename should be specified');
                 }
@@ -25,36 +28,39 @@ class CommandLineParser
                 if ((strlen($filename) < 1) || ($filename[0] == '-')) {
                     throw new Exception('CSV filename should not be empty and should not start with -');
                 }
-                $this->options['--file'] = $filename;
-            } elseif ($argv[$i] == '--create_table') {
-                $this->options['--create_table'] = true;
-            } elseif ($argv[$i] == '--dry_run') {
-                $this->options['--dry_run'] = true;
-            } elseif ($argv[$i] == '--help') {
+                $this->options[CSV_FILE_OPTION] = $filename;
+            } elseif ($argv[$i] == CREATE_TABLE_OPTION) {
+                $this->options[CREATE_TABLE_OPTION] = true;
+            } elseif ($argv[$i] == DRY_RUN_OPTION) {
+                $this->options[DRY_RUN_OPTION] = true;
+            } elseif ($argv[$i] == HELP_OPTION) {
                 throw new Exception('', -1);
-            } elseif ($this->assignShortOption($i, '-h', 'MySQL host')) {
-            } elseif ($this->assignShortOption($i, '-u', 'MySQL username')) {
-            } elseif ($this->assignShortOption($i, '-p', 'MySQL password')) {
+            } elseif ($this->assignShortOption($i, '-h')) {
+            } elseif ($this->assignShortOption($i, '-u')) {
+            } elseif ($this->assignShortOption($i, '-p')) {
             } else {
-                throw new Exception('unknown option in the command line: '.$argv[$i]);
+                throw new Exception('unknown option in the command line: '.$argv[$i].', run the script with '.HELP_OPTION.' option');
             }
         }
-        if (isset($this->options['--create_table'])) {
-            if (isset($this->options['--dry_run'])) {
-                throw new Exception('you cannot run --create_table in --dry_run mode');
+        if (isset($this->options[CREATE_TABLE_OPTION])) {
+            if (isset($this->options[DRY_RUN_OPTION])) {
+                throw new Exception('you cannot use '.CREATE_TABLE_OPTION.' in '.DRY_RUN_OPTION.' mode');
             }
-            if (isset($this->options['--file'])) {
-                throw new Exception('you cannot run --create_table together with CSV file upload');
+            if (isset($this->options[CSV_FILE_OPTION])) {
+                throw new Exception('you cannot use '.CREATE_TABLE_OPTION.' together with CSV file upload');
             }
             $this->checkShortOptions();
-        } elseif (!isset($this->options['--file'])) {
-            throw new Exception('either --file or --create_table should be specified');
+        } elseif (!isset($this->options[CSV_FILE_OPTION])) {
+            throw new Exception('either '.CSV_FILE_OPTION.' or '.CREATE_TABLE_OPTION.' should be specified');
+        } elseif (!isset($this->options[DRY_RUN_OPTION])) {
+            $this->checkShortOptions();
         }
     }
 
-    private function assignShortOption(&$i, $optionName, $optionMeaning)
+    private function assignShortOption(&$i, $optionName)
     {
         if ($this->argv[$i] == $optionName) {
+            $optionMeaning = $this->shortOptions[$optionName];
             if ($i + 1 == $this->argc) {
                 throw new Exception("$optionMeaning should be specified");
             }
@@ -75,16 +81,19 @@ class CommandLineParser
 
     private function checkShortOptions()
     {
-        foreach ($this->shortOptions as $option) {
-            if (!isset($this->options[$option[0]])) {
-                throw new Exception($option[1].' should be specified');
+        foreach ($this->shortOptions as $optionName => $optionMeaning) {
+            if (!isset($this->options[$optionName])) {
+                throw new Exception($optionMeaning.' should be specified');
             }
         }
     }
 
-    public static function displayHelp()
+    public static function displayHelp($scriptName)
     {
         $helpMessage = <<<HELP
+Usage: php {$scriptName} [OPTIONS]
+
+The options are:
 --file [csv file name] - this is the name of the CSV file to be parsed
 --dry_run - this can be used with the --file directive in case you want to run
             the script, but not insert into the DB. All other functions
@@ -96,7 +105,7 @@ class CommandLineParser
 -h - MySQL host
 --help - this will output the above list of directives
 HELP;
-        echo "\n".$helpMessage."\n";
+        echo $helpMessage."\n\n";
     }
 
     public function printOptions()
@@ -107,6 +116,21 @@ HELP;
     public function getOptions()
     {
         return $this->options;
+    }
+
+    public function isCreateTableMode()
+    {
+        return isset($this->options[CREATE_TABLE_OPTION]);
+    }
+
+    public function isDryRunMode()
+    {
+        return isset($this->options[DRY_RUN_OPTION]);
+    }
+
+    public function getCSVFilePath()
+    {
+        return isset($this->options[CSV_FILE_OPTION]) ? $this->options[CSV_FILE_OPTION] : '';
     }
 
 }
